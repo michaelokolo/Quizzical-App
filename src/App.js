d207ce7a5ef21051895cdae14afdf9bad5f9ac58
michaelokolo <91018269+michaelokolo@users.noreply.github.com>
2023-05-02 10:32:22 +0100

    completed the UI and commenced the functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
+import Question from './Question';
 
 export default function App() {
+  const [questionAnswer, setQuestionAnswer] = React.useState({});
+
+  React.useEffect(() => {
+    fetch('https://opentdb.com/api.php?amount=5&type=multiple')
+      .then((res) => res.json())
+      .then((data) => setQuestionAnswer(data));
+  }, []);
+
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div className="container">
+      <div>
+        <Question />
+        <Question />
+        <Question />
+        <Question />
+        <Question />
+      </div>
+      <button>Check answers</button>
     </div>
   );
 }
---
+++ src/Question.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Question(){
+  return(
+    <div className="question-container">
+      <h3 className="question">How would one say goodbye in spanish?</h3>
+      <div className="options">
+          <p>Adiós</p>
+          <p>Hola</p>
+          <p>Au Revoir</p>
+          <p>Salir</p>
+      </div>
+    </div>
+  )
+}
+
+export default Question
