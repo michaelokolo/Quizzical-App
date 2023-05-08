15bc3079326909334b6dea52a246941d92b2a554
michaelokolo <91018269+michaelokolo@users.noreply.github.com>
2023-05-08 14:30:03 +0100

    completed the check answer section

--- src/Question.js
+++ src/Question.js
@@ -4,26 +4,37 @@ import { nanoid } from 'nanoid';
 function Question(props) {
   const answers = props.question.answers;
   const answerElement = answers.map((answer) => {
-    const beforeChecked =()=>{
-      if (props.selected_answer === answer){
-        return 'answer'
-      }else{
-        return ''
+    const beforeChecked = () => {
+      if (props.selected_answer === answer) {
+        return 'answer';
+      } else {
+        return '';
       }
-    }
-    const afterChecked = ()=>{
-      if (props.selected_answer === answer && props.selected_answer === props.correct){
-        return 'correct'
-      }else if ((props.selected_answer === answer) && props.selected_answer !== props.correct){
-        return 'wrong'
-      }else if (props.selected_answer !== answer && props.selected_answer !== props.correct){
-        return ''
+    };
+    const afterChecked = () => {
+      if (
+        props.selected_answer === answer &&
+        props.selected_answer === props.correct
+      ) {
+        return 'correct';
+      } else if (
+        props.selected_answer === answer &&
+        props.selected_answer !== props.correct
+      ) {
+        return 'wrong';
+      } else if (props.selected_answer !== answer && props.correct === answer) {
+        return 'correct';
+      } else if (
+        props.selected_answer !== answer &&
+        props.selected_answer !== props.correct
+      ) {
+        return '';
       }
-    }
-  
+    };
+
     return (
       <p
-        className={!props.answerCheck ? beforeChecked():afterChecked()}
+        className={!props.answerCheck ? beforeChecked() : afterChecked()}
         onClick={() => props.selected(answer, props.id)}
         key={nanoid()}
       >
